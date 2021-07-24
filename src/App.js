f775ddc53385d3129cf52ea515e21f93566bbce2
facundobacci <facundobacci@cgp.gba.gov.ar>
2021-07-23 22:59:37 -0300

    Inicializacion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import {makeStyles} from "@material-ui/core";
+
+
+
 function App() {
-  
+  const classes = useStyles();
   return (
-    <div className="App">
+    <div className={classes.root}>
       <h1>Facundo Bacci</h1>
     </div>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+root:{
+  
+}
+
+}))
 
 export default App;
